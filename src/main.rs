13b75e250097f1521a7b3f3563f47d6ede0b7fe2
PhoenixGS <thestarrydream@gmail.com>
2022-08-24 20:29:21 +0800

    Version 0.1.3
    添加了多次游戏功能和统计功能，可以通过第四个test
    TODO：多次游戏随机选项不重复的功能

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,109 @@
 use console;
+use std::mem;
 use rand::Rng;
-use std::{io::{self, Write}, vec};
+use std::{io::{self, Write}, vec, mem::swap};
 
 mod builtin_words;
+
+struct Stats
+{
+    count: Vec<i32>,
+    list: Vec<usize>,
+}
+
+fn min(x: i32, y: i32) -> i32
+{
+    if x < y
+    {
+        return x;
+    }
+    else
+    {
+        return y;
+    }
+}
+
+fn max(x: i32, y: i32) -> i32
+{
+    if x > y
+    {
+        return x;
+    }
+    else
+    {
+        return y;
+    }
+}
+
+impl Stats
+{
+    fn get_id(guess: &String) -> usize
+    {
+        for i in 0..builtin_words::ACCEPTABLE.len()
+        {
+            if builtin_words::ACCEPTABLE[i].to_string().to_ascii_uppercase() == (*guess)
+            {
+                return i;
+            }
+        }
+        panic!("ERROR");
+    }
+
+    fn update(&mut self, guess: &String) -> ()
+    {
+        let index = Stats::get_id(guess);
+        self.count[index] += 1;
+        let mut now: usize = usize::MAX;
+        for i in 0..self.list.len()
+        {
+            if self.list[i] == index
+            {
+                now = i;
+                break;
+            }
+        }
+        if now == usize::MAX
+        {
+            if self.list.len() < 6
+            {
+                self.list.push(index);
+                now = self.list.len() - 1;
+            }
+            else
+            {
+                self.list[5] = index;
+                now = 5;
+            }
+        }
+        while now > 0
+        {
+            if (self.count[self.list[now]], -(self.list[now] as i32)) > (self.count[self.list[now - 1]], -(self.list[now - 1] as i32))
+            {
+                let t = self.list[now];
+                self.list[now] = self.list[now - 1];
+                self.list[now - 1] = t;
+                now -= 1;
+            }
+            else
+            {
+                break;
+            }
+        }
+    }
+
+    fn print(&mut self) -> ()
+    {
+        for i in 0..min(self.list.len() as i32, 5) as usize
+        {
+            if i != 0
+            {
+                print!(" ");
+            }
+            print!("{} {}", builtin_words::ACCEPTABLE[self.list[i]].to_string().to_ascii_uppercase(), self.count[self.list[i]]);
+        }
+        println!("");
+    }
+}
 
 fn print_c(st: String, c: char) -> () //Make the output colored
 {
@@ -11,7 +112,7 @@ fn print_c(st: String, c: char) -> () //Make the output colored
         'G' => print!("{}", console::style(st).green()),
         'Y' => print!("{}", console::style(st).yellow()),
         'R' => print!("{}", console::style(st).red()),
-        'X' => print!("{}", console::style(st).red()),
+        'X' => print!("{}", console::style(st).blue()),
         _ => ()
     }
 }
@@ -51,13 +152,21 @@ fn check_diffcult(pre_delta: &Vec<i32>, delta: &Vec<i32>, pre_out: &Vec<char>, o
 /// The main function for the Wordle game, implement your own logic here
 fn main() -> Result<(), Box<dyn std::error::Error>>
 {
+    let mut stats = Stats{count: vec![0; builtin_words::ACCEPTABLE.len()], list: vec![]};
+
     let is_tty = atty::is(atty::Stream::Stdout);
 
     let mut word = String::new();
 
+    //命令行参数
     let mut is_random = false;
     let mut is_word = false;
     let mut is_difficult = false;
+    let mut is_stats = false;
+
+    let mut game_cnt = 0;
+    let mut success_cnt = 0;
+    let mut success_try_cnt = 0;
 
     //Handle command line options
     let mut pre: String = "".to_string();
@@ -70,14 +179,16 @@ fn main() -> Result<(), Box<dyn std::error::Error>>
         }
         if arg == "-r".to_string() || arg == "--random".to_string()
         {
-            let index = rand::thread_rng().gen_range(0..builtin_words::FINAL.len());
-            word = String::from(builtin_words::FINAL[index]);
             is_random = true;
 //            println!("{}", word);
         }
         if arg == "-D".to_string() || arg == "--difficult".to_string()
         {
             is_difficult = true;
+        }
+        if arg == "-t".to_string() || arg == "--stats".to_string()
+        {
+            is_stats = true;
         }
         pre = String::from(&arg);
     }
@@ -102,151 +213,176 @@ fn main() -> Result<(), Box<dyn std::error::Error>>
         println!("Welcome to wordle, {}!", line.trim());
     }
 
-    let mut word_vec = vec![0; 26];
-    if ! is_random && ! is_word
+    while true
     {
-        io::stdin().read_line(&mut word)?;
-    }
-    word = word.trim().to_string().to_ascii_uppercase();
-//    println!("{}", word);
-    for i in 0..5
-    {
-        let c = word.chars().nth(i).unwrap();
-        word_vec[c as usize - 'A' as usize] += 1;
-    }
-//    println!("{:?}", word_vec);
+        game_cnt += 1;
 
-    let mut status = vec!['X'; 26];
-    let mut count = 0;
-    let mut success = false;
-    let mut res = vec![];
-    let mut words = vec![];
-    let mut pre_delta = vec![];
-    while count < 6
-    {
-        let mut guess = String::new();
-        let mut guess_vec = vec![0; 26];
-        io::stdin().read_line(&mut guess)?;
-        guess = guess.trim().to_string().to_ascii_uppercase();
-        if ! check(&guess)
+        let mut word_vec = vec![0; 26];
+        if is_random
         {
-            println!("INVALID");
-            continue;
+            //TODO 保证每次不重复
+            let index = rand::thread_rng().gen_range(0..builtin_words::FINAL.len());
+            word = String::from(builtin_words::FINAL[index]);
         }
+        if ! is_random && ! is_word
+        {
+            word = String::new();
+            io::stdin().read_line(&mut word)?;
+        }
+        word = word.trim().to_string().to_ascii_uppercase();
         for i in 0..5
         {
-            let c = guess.chars().nth(i).unwrap();
-            guess_vec[c as usize - 'A' as usize] += 1;
+            let c = word.chars().nth(i).unwrap();
+            word_vec[c as usize - 'A' as usize] += 1;
         }
-        let mut delta = vec![0; 26];
-        for i in 0..26
+    //    println!("{:?}", word_vec);
+
+        let mut status = vec!['X'; 26];
+        let mut count = 0;
+        let mut success = false;
+        let mut res = vec![];
+        let mut words = vec![];
+        let mut pre_delta = vec![];
+        while count < 6
         {
-            delta[i] = word_vec[i];
-        }
-        let mut out = vec!['S'; 5];
-        let mut cnt = 0;
-        let mut new_status = status.clone();
-        for i in 0..5
-        {
-            if word.chars().nth(i) == guess.chars().nth(i)
+            let mut guess = String::new();
+            let mut guess_vec = vec![0; 26];
+            io::stdin().read_line(&mut guess)?;
+            guess = guess.trim().to_string().to_ascii_uppercase();
+            if ! check(&guess)
             {
-                out[i] = 'G';
-                let index = guess.chars().nth(i).unwrap() as usize - 'A' as usize;
-                delta[index] -= 1;
-                cnt += 1;
-                new_status[index] = 'G';
+                println!("INVALID");
+                continue;
             }
-        }
-        for i in 0..5
-        {
-            if out[i] != 'G'
-            {
-                let index = guess.chars().nth(i).unwrap() as usize - 'A' as usize;
-                if delta[index] > 0
-                {
-                    delta[index] -= 1;
-                    out[i] = 'Y';
-                    if new_status[index] != 'G'
-                    {
-                        new_status[index] = 'Y';
-                    }
-                }
-                else
-                {
-                    out[i] = 'R';
-                    if new_status[index] != 'G' && new_status[index] != 'Y'
-                    {
-                        new_status[index] = 'R';
-                    }
-                }
-            }
-        }
-
-        if count > 0 && is_difficult && ! check_diffcult(&pre_delta, &delta, &res[count - 1], &out)
-        {
-            println!("INVALID");
-            continue;
-        }
-
-        count += 1;
-        status = new_status.clone();
-
-        res.push(out);
-        words.push(guess);
-        if ! is_tty
-        {
             for i in 0..5
             {
-                print!("{}", res[count - 1][i]);
+                let c = guess.chars().nth(i).unwrap();
+                guess_vec[c as usize - 'A' as usize] += 1;
             }
-            print!(" ");
+            let mut delta = vec![0; 26];
             for i in 0..26
             {
-                print!("{}", status[i]);
+                delta[i] = word_vec[i];
             }
-            println!("");
-        }
-        else
-        {
-            for i in 0..count
+            let mut out = vec!['S'; 5];
+            let mut cnt = 0;
+            let mut new_status = status.clone();
+            for i in 0..5
             {
-                for j in 0..5
+                if word.chars().nth(i) == guess.chars().nth(i)
                 {
-                    print_c(words[i][j..j + 1].to_string(), res[i][j]);
+                    out[i] = 'G';
+                    let index = guess.chars().nth(i).unwrap() as usize - 'A' as usize;
+                    delta[index] -= 1;
+                    cnt += 1;
+                    new_status[index] = 'G';
+                }
+            }
+            for i in 0..5
+            {
+                if out[i] != 'G'
+                {
+                    let index = guess.chars().nth(i).unwrap() as usize - 'A' as usize;
+                    if delta[index] > 0
+                    {
+                        delta[index] -= 1;
+                        out[i] = 'Y';
+                        if new_status[index] != 'G'
+                        {
+                            new_status[index] = 'Y';
+                        }
+                    }
+                    else
+                    {
+                        out[i] = 'R';
+                        if new_status[index] != 'G' && new_status[index] != 'Y'
+                        {
+                            new_status[index] = 'R';
+                        }
+                    }
+                }
+            }
+
+            if count > 0 && is_difficult && ! check_diffcult(&pre_delta, &delta, &res[count - 1], &out)
+            {
+                println!("INVALID");
+                continue;
+            }
+
+            stats.update(&guess);
+
+            count += 1;
+            status = new_status.clone();
+
+            res.push(out);
+            words.push(guess);
+            if ! is_tty
+            {
+                for i in 0..5
+                {
+                    print!("{}", res[count - 1][i]);
+                }
+                print!(" ");
+                for i in 0..26
+                {
+                    print!("{}", status[i]);
                 }
                 println!("");
             }
-            for i in 0..26
+            else
             {
-                print_c(((i as u8 + 65) as char).to_string(), status[i]);
+                for i in 0..count
+                {
+                    for j in 0..5
+                    {
+                        print_c(words[i][j..j + 1].to_string(), res[i][j]);
+                    }
+                    println!("");
+                }
+                for i in 0..26
+                {
+                    print_c(((i as u8 + 65) as char).to_string(), status[i]);
+                }
+                println!("");
             }
-            println!("");
+            if cnt == 5
+            {
+                success = true;
+                break;
+            }
+            pre_delta = delta.clone();
         }
-        if cnt == 5
+
+        if success
         {
-            success = true;
+            println!("CORRECT {}", count);
+            success_cnt += 1;
+            success_try_cnt += count;
+        }
+        else
+        {
+            println!("FAILED {}", word);
+        }
+
+        if is_stats
+        {
+            println!("{} {} {:.2}", success_cnt, game_cnt - success_cnt, success_try_cnt as f32 / max(1, success_cnt) as f32);
+            stats.print();
+        }
+
+        if ! is_word
+        {
+            let mut next_game = String::new();
+            if io::stdin().read_line(&mut next_game).unwrap() == 0 || next_game.trim().to_string() == "N".to_string()
+            {
+                break;
+            }
+        }
+        else
+        {
             break;
         }
-        pre_delta = delta.clone();
     }
-
-    if success
-    {
-        println!("CORRECT {}", count);
-    }
-    else
-    {
-        println!("FAILED {}", word);
-    }
-
-    // example: print arguments
-/*    print!("Command line arguments: ");
-    for arg in std::env::args()
-    {
-        print!("{} ", arg);
-    }
-    println!("");*/
-    // TODO: parse the arguments in `args`
 
     Ok(())
 }
