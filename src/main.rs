9634c88142900c41f24f635524ba321dcdf5984b
PhoenixGS <thestarrydream@gmail.com>
2022-08-24 15:47:49 +0800

    Version 0.1.0
    添加了Wordle的基本功能，可以通过第一个test

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,199 @@
 use console;
 use std::io::{self, Write};
 
-/// The main function for the Wordle game, implement your own logic here
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let is_tty = atty::is(atty::Stream::Stdout);
+mod builtin_words;
 
-    if is_tty {
+fn print_c(st: String, c: char) -> ()
+{
+    match c
+    {
+        'G' => print!("{}", console::style(st).green()),
+        'Y' => print!("{}", console::style(st).yellow()),
+        'R' => print!("{}", console::style(st).red()),
+        'X' => print!("{}", console::style(st).red()),
+        _ => ()
+    }
+}
+
+fn check(guess: &String) -> bool
+{
+    for s in builtin_words::ACCEPTABLE
+    {
+//        println!("{} {}", (*s).to_string().to_ascii_uppercase(), (*guess));
+        if (*s).to_string().to_ascii_uppercase() == (*guess)
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+/// The main function for the Wordle game, implement your own logic here
+fn main() -> Result<(), Box<dyn std::error::Error>>
+{
+    let is_tty = atty::is(atty::Stream::Stdout);
+    //
+    let is_random = false;
+
+    if is_tty
+    {
         println!(
             "I am in a tty. Please print {}!",
             console::style("colorful characters").bold().blink().blue()
         );
-    } else {
-        println!("I am not in a tty. Please print according to test requirements!");
+    }
+    else
+    {
+//        println!("I am not in a tty. Please print according to test requirements!");
     }
 
-    if is_tty {
+    if is_tty
+    {
         print!("{}", console::style("Your name: ").bold().red());
         io::stdout().flush().unwrap();
+        let mut line = String::new();
+        io::stdin().read_line(&mut line)?;
+        println!("Welcome to wordle, {}!", line.trim());
     }
-    let mut line = String::new();
-    io::stdin().read_line(&mut line)?;
-    println!("Welcome to wordle, {}!", line.trim());
+
+    let mut word = String::new();
+    let mut word_vec = vec![0; 26];
+    if ! is_random
+    {
+        io::stdin().read_line(&mut word)?;
+    }
+    else
+    {
+        word = "ALPHA".to_string();
+        //
+    }
+    word = word.trim().to_string().to_ascii_uppercase();
+//    println!("{}", word);
+    for i in 0..5
+    {
+        let c = word.chars().nth(i).unwrap();
+        word_vec[c as usize - 'A' as usize] += 1;
+    }
+//    println!("{:?}", word_vec);
+
+    let mut status = vec!['X'; 26];
+    let mut count = 0;
+    let mut success = false;
+    let mut res = vec![];
+    let mut words = vec![];
+    while count < 6
+    {
+        let mut guess = String::new();
+        let mut guess_vec = vec![0; 26];
+        io::stdin().read_line(&mut guess)?;
+        guess = guess.trim().to_string().to_ascii_uppercase();
+        if ! check(&guess)
+        {
+            println!("INVALID");
+            continue;
+        }
+        count += 1;
+        for i in 0..5
+        {
+            let c = guess.chars().nth(i).unwrap();
+            guess_vec[c as usize - 'A' as usize] += 1;
+        }
+        let mut delta = vec![0; 26];
+        for i in 0..26
+        {
+            delta[i] = word_vec[i];
+        }
+        let mut out = vec!['S'; 5];
+        let mut cnt = 0;
+        for i in 0..5
+        {
+            if word.chars().nth(i) == guess.chars().nth(i)
+            {
+                out[i] = 'G';
+                let index = guess.chars().nth(i).unwrap() as usize - 'A' as usize;
+                delta[index] -= 1;
+                cnt += 1;
+                status[index] = 'G';
+            }
+        }
+        for i in 0..5
+        {
+            if out[i] != 'G'
+            {
+                let index = guess.chars().nth(i).unwrap() as usize - 'A' as usize;
+                if delta[index] > 0
+                {
+                    delta[index] -= 1;
+                    out[i] = 'Y';
+                    if status[index] != 'G'
+                    {
+                        status[index] = 'Y';
+                    }
+                }
+                else
+                {
+                    out[i] = 'R';
+                    if status[index] != 'G' && status[index] != 'Y'
+                    {
+                        status[index] = 'R';
+                    }
+                }
+            }
+        }
+        res.push(out);
+        words.push(guess);
+        if ! is_tty
+        {
+            for i in 0..5
+            {
+                print!("{}", res[count - 1][i]);
+            }
+            print!(" ");
+            for i in 0..26
+            {
+                print!("{}", status[i]);
+            }
+            println!("");
+        }
+        else
+        {
+            for i in 0..count
+            {
+                for j in 0..5
+                {
+                    print_c(words[i][j..j + 1].to_string(), res[i][j]);
+                }
+                println!("");
+            }
+            for i in 0..26
+            {
+                print_c(((i as u8 + 65) as char).to_string(), status[i]);
+            }
+            println!("");
+        }
+        if cnt == 5
+        {
+            success = true;
+            break;
+        }
+    }
+
+    if success
+    {
+        println!("CORRECT {}", count);
+    }
+    else
+    {
+        println!("FAILED {}", word);
+    }
 
     // example: print arguments
-    print!("Command line arguments: ");
-    for arg in std::env::args() {
+/*    print!("Command line arguments: ");
+    for arg in std::env::args()
+    {
         print!("{} ", arg);
     }
-    println!("");
+    println!("");*/
     // TODO: parse the arguments in `args`
 
     Ok(())
